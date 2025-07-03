b4a6a4f0c545b2948595ef6e5d777a142ff13015
muhafs <muhamadhaspin@gmail.com>
2025-07-03 13:13:53 +0700

    add dashboard layout: - menu - navbar

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,35 @@
+import Menu from '@/components/Menu'
+import Navbar from '@/components/Navbar'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function DashboardLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<div className="h-min-screen flex">
+			{/* Left */}
+			<div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+				{/* Logo */}
+				<Link href="/" className="flex items-center justify-center gap-2 lg:justify-start">
+					<Image src="/logo.png" alt="Logo" width={32} height={32} />
+					<span className="hidden lg:block">SchoolName</span>
+				</Link>
+
+				{/* Menu */}
+				<Menu />
+			</div>
+
+			{/* Right */}
+			<div className="bg-[#F7F8FA] w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] overflow-scroll">
+				{/* Navbar */}
+				<Navbar />
+
+				{/* Content */}
+				{children}
+			</div>
+		</div>
+	)
+}
