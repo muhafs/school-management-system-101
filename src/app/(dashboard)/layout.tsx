84094214886e6df8dddc1bb7ea7dbc588a7b8945
muhafs <muhamadhaspin@gmail.com>
2025-07-04 19:23:05 +0700

    add student page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -23,7 +23,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
 			</div>
 
 			{/* Right */}
-			<div className="bg-[#F7F8FA] w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] overflow-scroll">
+			<div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-auto flex flex-col">
 				{/* Navbar */}
 				<Navbar />
 
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,5 +1,26 @@
+import Announcement from '@/components/Announcement'
+import BigCalendar from '@/components/Calendar/BigCalendar'
+import EventCalendar from '@/components/Calendar/EventCalendar'
+
 const StudentPage = () => {
-	return <div className="">StudentPage</div>
+	return (
+		<div className="flex flex-1 flex-col xl:flex-row p-4 gap-4">
+			{/* Left */}
+			<div className="w-full xl:w-2/3">
+				<div className="h-full bg-white p-4 rounded-md">
+					<h1 className="text-xl font-semibold">Schedule (4A)</h1>
+					<BigCalendar />
+				</div>
+			</div>
+
+			{/* Right */}
+			<div className="w-full xl:w-1/3 flex flex-col gap-8">
+				<EventCalendar />
+
+				<Announcement />
+			</div>
+		</div>
+	)
 }
 
 export default StudentPage
